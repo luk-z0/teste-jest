7997f8fa4c062c3a3e04de4dec720987b3f7e732
Lucas Gabriel <lucas1020111940@hotmail.com>
2025-04-02 15:12:48 -0300

    more test to empty or remove

--- src/cart/cart.spec.ts
+++ src/cart/cart.spec.ts
@@ -1,15 +1,62 @@
 import Product from "../products/product"
 import Cart from "./cart"
 
+const makeCart = (): Cart => {
+  return new Cart
+}
+
+const makeProduct = (name: string, price: number): Product => {
+  return new Product(name, price)
+}
+
+const makeSut = () => {
+  const sut = makeCart()
+
+  return {
+    sut
+  }
+}
+
 describe('Cart', () => {
-  it.only('should empty cart', () => {
-    const cart = new Cart
-    expect(cart.items.length).toBe(0)
+  it('should empty cart', () => {
+    const { sut } = makeSut()
+    expect(sut.items.length).toBe(0)
   })
 
   it('should has one item in cart', () => {
-    const cart = new Cart
-    const product = new Product('product', 10)
-    cart.addItem(product)
+    const { sut } = makeSut()
+    sut.addItem(makeProduct('carro', 10))
+  })
+
+  it('should has two item in cart', () => {
+    const { sut } = makeSut()
+    sut.addItem(makeProduct('carro', 10))
+    sut.addItem(makeProduct('boneca', 25))
+  })
+
+  it('should have one item if add two and remove one', () => {
+    const { sut } = makeSut()
+    const product = makeProduct('algema', 100)
+
+    sut.addItem(product)
+    sut.addItem(makeProduct('arma', 2000))
+
+    expect(sut.items.length).toBe(2)
+    sut.removeItem(product)
+    expect(sut.items.length).toBe(1)
+  })
+
+  it('should empty if add two and remove one', () => {
+    const { sut } = makeSut()
+    const product = makeProduct('algema', 100)
+    const productTwo = makeProduct('arma', 2000)
+
+    sut.addItem(product)
+    sut.addItem(makeProduct('arma', 2000))
+    expect(sut.items.length).toBe(2)
+    sut.removeItem(product)
+    sut.removeItem(productTwo)
+
+    expect(sut.isEmpty).toBeTruthy
   })
 })
